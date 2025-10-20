ebf2b72c3ce6bf08f5eede13a94364541b8e7b95
Joshua Mühlfort <muehlfort@gonicus.de>
2025-10-20 14:31:48 +0000

    add POC source code

---
+++ src/main.rs
@@ -0,0 +1,78 @@
+mod images;
+mod nixstores;
+mod vms;
+
+use adw::prelude::AdwWindowExt;
+use gtk::{
+    glib::{self},
+    prelude::*,
+};
+
+#[derive(PartialEq, Debug)]
+struct State {
+    images: Vec<images::Image>,
+    vms: Vec<vms::VM>,
+    nixstores: Vec<nixstores::NixStore>,
+}
+
+fn load_state() -> State {
+    return State {
+        images: images::load_images(),
+        vms: vms::load_vms(),
+        nixstores: nixstores::load_nixstores(),
+    };
+}
+fn build_window_content() -> gtk::Widget {
+    let state = load_state();
+    let toolbar_view = adw::ToolbarView::builder().build();
+    let view_stack = adw::ViewStack::builder().name("view").build();
+    view_stack.add_titled_with_icon(
+        &images::build_image_list(state.images),
+        Some("images"),
+        "Images",
+        "drive-harddisk-system-symbolic",
+    );
+    view_stack.add_titled_with_icon(
+        &nixstores::build_nixstore_list(state.nixstores),
+        Some("nixstores"),
+        "Nix Stores",
+        "view-app-grid-symbolic",
+    );
+    view_stack.add_titled_with_icon(
+        &vms::build_vms_list(state.vms),
+        Some("vms"),
+        "Bubbles",
+        "computer-symbolic",
+    );
+    let view_switcher = adw::ViewSwitcher::builder()
+        .stack(&view_stack)
+        .policy(adw::ViewSwitcherPolicy::Wide)
+        .build();
+    let top_bar = adw::HeaderBar::builder()
+        .title_widget(&view_switcher)
+        .build();
+    toolbar_view.add_top_bar(&top_bar);
+    toolbar_view.set_content(Some(&view_stack));
+    return toolbar_view.into();
+}
+
+fn build_ui(app: &adw::Application) {
+    let window = adw::Window::builder()
+        .default_width(600)
+        .default_height(600)
+        .application(app)
+        .title("Bubbles")
+        .build();
+
+    window.set_content(Some(&build_window_content()));
+    window.present();
+}
+
+fn main() -> glib::ExitCode {
+    let application = adw::Application::builder()
+        .application_id("de.gonicus.Bubbles")
+        .build();
+
+    application.connect_activate(build_ui);
+    application.run()
+}
