8b50a16a02305823d98b401e463c41d6c472763b
Joshua Mühlfort <muehlfort@gonicus.de>
2026-03-19 15:46:49 +0100

    app: update vm image

--- bubbles-app/scripts/download.bash
+++ bubbles-app/scripts/download.bash
@@ -8,7 +8,7 @@ BUBBLES_DIR=$PWD
 mkdir -p $TARGET_DIRECTORY
 cd $TARGET_DIRECTORY
 
-$BUBBLES_DIR/oras pull ghcr.io/gonicus/bubbles/vm-image:6c68cf5d36ed12bbb2955407270708f0f0fa7b2c
+$BUBBLES_DIR/oras pull ghcr.io/gonicus/bubbles/vm-image:b94678cf8785b82f0147f9a3fd5d5220c0b981a4
 
 qemu-img convert -f qcow2 -O raw disk.qcow2 disk.img
 truncate -s +15G disk.img
